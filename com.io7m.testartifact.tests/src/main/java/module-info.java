4a281942f96aca72c9ada792079d6cf8687c39ff
Mark Raynsford <code@io7m.com>
2024-10-26 13:34:01 +0000

    Use junit BOM.

--- com.io7m.testartifact.tests/src/main/java/module-info.java
+++ com.io7m.testartifact.tests/src/main/java/module-info.java
@@ -22,8 +22,9 @@ open module com.io7m.testartifact.tests
 {
   requires com.io7m.testartifact.core;
 
-  requires transitive org.junit.jupiter.api;
-  requires transitive org.junit.jupiter.engine;
-  requires transitive org.junit.platform.commons;
-  requires transitive org.junit.platform.engine;
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.engine;
+  requires org.junit.platform.commons;
+  requires org.junit.platform.engine;
+  requires org.junit.platform.launcher;
 }
